fcff16e95b45b733f98e49fa7aaf244ff7c34848
SojethRajan <sojetha@gmail.com>
2023-12-27 20:07:24 +0530

    modified index.js for demo purpose

--- index.js
+++ index.js
@@ -1,18 +1,1 @@
-function getPassingTests(results) {
-    results = results.filter((result) => {
-        return result.grade >= 10;
-    })
-    return results;
-}
-// sample usage (do not modify)
-const data = [{
-    id: 1,
-    grade: 10
-}, {
-    id: 2,
-    grade: 4
-}, {
-    id: 3,
-    grade: 18
-}]
-console.log(getPassingTests(data))
+console.l('hello world')
